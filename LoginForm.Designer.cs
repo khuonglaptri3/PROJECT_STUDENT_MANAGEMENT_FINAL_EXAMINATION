2e358c3442cd49627d53185cdb8a5f2a2db77a6e
Dinh Khuong <lanphuongtran1999@gmail.com>
2024-11-26 12:13:31 +0700

    Form login

--- LoginForm.Designer.cs
+++ LoginForm.Designer.cs
@@ -29,17 +29,17 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.Username = new System.Windows.Forms.TextBox();
-            this.Password = new System.Windows.Forms.TextBox();
-            this.Loginbut = new System.Windows.Forms.Button();
             this.showpass = new System.Windows.Forms.CheckBox();
+            this.Loginbut = new System.Windows.Forms.Button();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.Username = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -57,15 +57,15 @@
             this.panel1.Size = new System.Drawing.Size(550, 30);
             this.panel1.TabIndex = 0;
             // 
-            // panel2
+            // label2
             // 
-            this.panel2.BackColor = System.Drawing.Color.PaleGreen;
-            this.panel2.Controls.Add(this.panel3);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(0, 30);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(550, 570);
-            this.panel2.TabIndex = 1;
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 2);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(316, 24);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "School Management System | Login";
             // 
             // label1
             // 
@@ -78,15 +78,16 @@
             this.label1.Text = "X";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // label2
+            // panel2
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(3, 2);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(316, 24);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "School Management System | Login";
+            this.panel2.BackColor = System.Drawing.Color.PaleGreen;
+            this.panel2.Controls.Add(this.panel3);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 30);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(550, 570);
+            this.panel2.TabIndex = 1;
+            this.panel2.Paint += new System.Windows.Forms.PaintEventHandler(this.panel2_Paint);
             // 
             // panel3
             // 
@@ -98,32 +99,54 @@
             this.panel3.Controls.Add(this.label5);
             this.panel3.Controls.Add(this.label4);
             this.panel3.Controls.Add(this.label3);
-            this.panel3.Location = new System.Drawing.Point(110, 81);
+            this.panel3.Location = new System.Drawing.Point(101, 72);
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(317, 421);
             this.panel3.TabIndex = 0;
             // 
-            // label3
+            // showpass
             // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.Black;
-            this.label3.Location = new System.Drawing.Point(107, 40);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(102, 31);
-            this.label3.TabIndex = 0;
-            this.label3.Text = "Sign In ";
+            this.showpass.AutoSize = true;
+            this.showpass.BackColor = System.Drawing.Color.DarkSeaGreen;
+            this.showpass.ForeColor = System.Drawing.Color.Black;
+            this.showpass.Location = new System.Drawing.Point(163, 286);
+            this.showpass.Name = "showpass";
+            this.showpass.Size = new System.Drawing.Size(125, 20);
+            this.showpass.TabIndex = 2;
+            this.showpass.Text = "Show Password";
+            this.showpass.UseVisualStyleBackColor = false;
+            this.showpass.CheckedChanged += new System.EventHandler(this.showpass_CheckedChanged);
             // 
-            // label4
+            // Loginbut
             // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.Black;
-            this.label4.Location = new System.Drawing.Point(13, 110);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(90, 19);
-            this.label4.TabIndex = 1;
-            this.label4.Text = "Username:";
+            this.Loginbut.BackColor = System.Drawing.Color.Teal;
+            this.Loginbut.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Loginbut.ForeColor = System.Drawing.Color.Black;
+            this.Loginbut.Location = new System.Drawing.Point(187, 323);
+            this.Loginbut.Name = "Loginbut";
+            this.Loginbut.Size = new System.Drawing.Size(87, 41);
+            this.Loginbut.TabIndex = 5;
+            this.Loginbut.Text = "Log in ";
+            this.Loginbut.UseVisualStyleBackColor = false;
+            this.Loginbut.Click += new System.EventHandler(this.Loginbut_Click);
+            // 
+            // Password
+            // 
+            this.Password.Font = new System.Drawing.Font("Microsoft YaHei", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Password.Location = new System.Drawing.Point(17, 230);
+            this.Password.Name = "Password";
+            this.Password.PasswordChar = '*';
+            this.Password.Size = new System.Drawing.Size(284, 31);
+            this.Password.TabIndex = 4;
+            this.Password.TextChanged += new System.EventHandler(this.Password_TextChanged);
+            // 
+            // Username
+            // 
+            this.Username.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Username.Location = new System.Drawing.Point(17, 148);
+            this.Username.Name = "Username";
+            this.Username.Size = new System.Drawing.Size(284, 29);
+            this.Username.TabIndex = 3;
             // 
             // label5
             // 
@@ -136,45 +159,27 @@
             this.label5.TabIndex = 2;
             this.label5.Text = "Password: ";
             // 
-            // Username
+            // label4
             // 
-            this.Username.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Username.Location = new System.Drawing.Point(17, 148);
-            this.Username.Name = "Username";
-            this.Username.Size = new System.Drawing.Size(284, 29);
-            this.Username.TabIndex = 3;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft YaHei UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Black;
+            this.label4.Location = new System.Drawing.Point(13, 110);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 19);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Username:";
             // 
-            // Password
+            // label3
             // 
-            this.Password.Location = new System.Drawing.Point(17, 230);
-            this.Password.Name = "Password";
-            this.Password.PasswordChar = '*';
-            this.Password.Size = new System.Drawing.Size(284, 22);
-            this.Password.TabIndex = 4;
-            // 
-            // Loginbut
-            // 
-            this.Loginbut.BackColor = System.Drawing.Color.Teal;
-            this.Loginbut.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Loginbut.ForeColor = System.Drawing.Color.Black;
-            this.Loginbut.Location = new System.Drawing.Point(187, 323);
-            this.Loginbut.Name = "Loginbut";
-            this.Loginbut.Size = new System.Drawing.Size(101, 41);
-            this.Loginbut.TabIndex = 5;
-            this.Loginbut.Text = "Log in ";
-            this.Loginbut.UseVisualStyleBackColor = false;
-            // 
-            // showpass
-            // 
-            this.showpass.AutoSize = true;
-            this.showpass.ForeColor = System.Drawing.Color.Black;
-            this.showpass.Location = new System.Drawing.Point(163, 286);
-            this.showpass.Name = "showpass";
-            this.showpass.Size = new System.Drawing.Size(125, 20);
-            this.showpass.TabIndex = 2;
-            this.showpass.Text = "Show Password";
-            this.showpass.UseVisualStyleBackColor = true;
-            this.showpass.CheckedChanged += new System.EventHandler(this.showpass_CheckedChanged);
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft YaHei UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Black;
+            this.label3.Location = new System.Drawing.Point(107, 40);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(102, 31);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Sign In ";
             // 
             // LoginForm
             // 
--- LoginForm.cs
+++ LoginForm.cs
@@ -26,5 +26,20 @@ namespace PROJECT_STUDENT_MANAGEMENT_FINAL_EXAMINATION
         {
             Password.PasswordChar = showpass.Checked ? '\0' : '*'; 
         }
+
+        private void Loginbut_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void Password_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
