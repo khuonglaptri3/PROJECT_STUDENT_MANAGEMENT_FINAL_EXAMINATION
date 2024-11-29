c67b28f398ebc8a75caf847e1e156778d185e226
Dinh Khuong <lanphuongtran1999@gmail.com>
2024-11-29 16:12:02 +0700

    Khuong

--- AdminForm.Designer.cs
+++ AdminForm.Designer.cs
@@ -28,22 +28,217 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel_slide = new System.Windows.Forms.Panel();
+            this.Panel_subtch = new System.Windows.Forms.Panel();
+            this.button_printtch = new System.Windows.Forms.Button();
+            this.button_managetch = new System.Windows.Forms.Button();
+            this.button_Registrationtch = new System.Windows.Forms.Button();
+            this.button_tch = new System.Windows.Forms.Button();
+            this.panel_substd = new System.Windows.Forms.Panel();
+            this.button_stdPrint = new System.Windows.Forms.Button();
+            this.button_manageStd = new System.Windows.Forms.Button();
+            this.button_registrationstd = new System.Windows.Forms.Button();
+            this.button_std = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel_slide.SuspendLayout();
+            this.Panel_subtch.SuspendLayout();
+            this.panel_substd.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel_slide
+            // 
+            this.panel_slide.BackColor = System.Drawing.Color.SpringGreen;
+            this.panel_slide.Controls.Add(this.Panel_subtch);
+            this.panel_slide.Controls.Add(this.button_tch);
+            this.panel_slide.Controls.Add(this.panel_substd);
+            this.panel_slide.Controls.Add(this.button_std);
+            this.panel_slide.Controls.Add(this.panel2);
+            this.panel_slide.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel_slide.Location = new System.Drawing.Point(0, 0);
+            this.panel_slide.Name = "panel_slide";
+            this.panel_slide.Size = new System.Drawing.Size(212, 689);
+            this.panel_slide.TabIndex = 0;
+            // 
+            // Panel_subtch
+            // 
+            this.Panel_subtch.Controls.Add(this.button_printtch);
+            this.Panel_subtch.Controls.Add(this.button_managetch);
+            this.Panel_subtch.Controls.Add(this.button_Registrationtch);
+            this.Panel_subtch.Dock = System.Windows.Forms.DockStyle.Top;
+            this.Panel_subtch.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Panel_subtch.Location = new System.Drawing.Point(0, 440);
+            this.Panel_subtch.Name = "Panel_subtch";
+            this.Panel_subtch.Size = new System.Drawing.Size(212, 204);
+            this.Panel_subtch.TabIndex = 3;
+            // 
+            // button_printtch
+            // 
+            this.button_printtch.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button_printtch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_printtch.Location = new System.Drawing.Point(0, 134);
+            this.button_printtch.Name = "button_printtch";
+            this.button_printtch.Size = new System.Drawing.Size(212, 67);
+            this.button_printtch.TabIndex = 5;
+            this.button_printtch.Text = "Print";
+            this.button_printtch.UseVisualStyleBackColor = true;
+            this.button_printtch.Click += new System.EventHandler(this.button_printtch_Click);
+            // 
+            // button_managetch
+            // 
+            this.button_managetch.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button_managetch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_managetch.Location = new System.Drawing.Point(0, 67);
+            this.button_managetch.Name = "button_managetch";
+            this.button_managetch.Size = new System.Drawing.Size(212, 67);
+            this.button_managetch.TabIndex = 4;
+            this.button_managetch.Text = "Manage Teacher";
+            this.button_managetch.UseVisualStyleBackColor = true;
+            this.button_managetch.Click += new System.EventHandler(this.button_managetch_Click);
+            // 
+            // button_Registrationtch
+            // 
+            this.button_Registrationtch.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button_Registrationtch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_Registrationtch.Location = new System.Drawing.Point(0, 0);
+            this.button_Registrationtch.Name = "button_Registrationtch";
+            this.button_Registrationtch.Size = new System.Drawing.Size(212, 67);
+            this.button_Registrationtch.TabIndex = 3;
+            this.button_Registrationtch.Text = "Registration";
+            this.button_Registrationtch.UseVisualStyleBackColor = true;
+            this.button_Registrationtch.Click += new System.EventHandler(this.button_Registrationtch_Click);
+            // 
+            // button_tch
+            // 
+            this.button_tch.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button_tch.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_tch.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.button_tch.Location = new System.Drawing.Point(0, 389);
+            this.button_tch.Name = "button_tch";
+            this.button_tch.Size = new System.Drawing.Size(212, 51);
+            this.button_tch.TabIndex = 3;
+            this.button_tch.Text = "Teacher";
+            this.button_tch.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button_tch.UseVisualStyleBackColor = true;
+            this.button_tch.Click += new System.EventHandler(this.button_tch_Click);
+            // 
+            // panel_substd
+            // 
+            this.panel_substd.Controls.Add(this.button_stdPrint);
+            this.panel_substd.Controls.Add(this.button_manageStd);
+            this.panel_substd.Controls.Add(this.button_registrationstd);
+            this.panel_substd.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel_substd.Font = new System.Drawing.Font("Microsoft Tai Le", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panel_substd.Location = new System.Drawing.Point(0, 185);
+            this.panel_substd.Name = "panel_substd";
+            this.panel_substd.Size = new System.Drawing.Size(212, 204);
+            this.panel_substd.TabIndex = 2;
+            // 
+            // button_stdPrint
+            // 
+            this.button_stdPrint.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button_stdPrint.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_stdPrint.Location = new System.Drawing.Point(0, 134);
+            this.button_stdPrint.Name = "button_stdPrint";
+            this.button_stdPrint.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button_stdPrint.Size = new System.Drawing.Size(212, 67);
+            this.button_stdPrint.TabIndex = 5;
+            this.button_stdPrint.Text = "Print";
+            this.button_stdPrint.UseVisualStyleBackColor = true;
+            this.button_stdPrint.Click += new System.EventHandler(this.button_stdPrint_Click);
+            // 
+            // button_manageStd
+            // 
+            this.button_manageStd.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button_manageStd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_manageStd.Location = new System.Drawing.Point(0, 67);
+            this.button_manageStd.Name = "button_manageStd";
+            this.button_manageStd.Size = new System.Drawing.Size(212, 67);
+            this.button_manageStd.TabIndex = 4;
+            this.button_manageStd.Text = "Manage Student";
+            this.button_manageStd.UseVisualStyleBackColor = true;
+            this.button_manageStd.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button_registrationstd
+            // 
+            this.button_registrationstd.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button_registrationstd.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_registrationstd.Location = new System.Drawing.Point(0, 0);
+            this.button_registrationstd.Name = "button_registrationstd";
+            this.button_registrationstd.Size = new System.Drawing.Size(212, 67);
+            this.button_registrationstd.TabIndex = 3;
+            this.button_registrationstd.Text = "Registration";
+            this.button_registrationstd.UseVisualStyleBackColor = true;
+            this.button_registrationstd.Click += new System.EventHandler(this.button_registrationstd_Click);
+            // 
+            // button_std
+            // 
+            this.button_std.Dock = System.Windows.Forms.DockStyle.Top;
+            this.button_std.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button_std.Font = new System.Drawing.Font("Microsoft Tai Le", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_std.Location = new System.Drawing.Point(0, 118);
+            this.button_std.Name = "button_std";
+            this.button_std.Size = new System.Drawing.Size(212, 67);
+            this.button_std.TabIndex = 1;
+            this.button_std.Text = "Student";
+            this.button_std.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button_std.UseVisualStyleBackColor = true;
+            this.button_std.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.SpringGreen;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(212, 118);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft YaHei UI", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(858, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(25, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "X";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // AdminForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(885, 554);
+            this.ClientSize = new System.Drawing.Size(885, 689);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel_slide);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "AdminForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "AdminForm";
             this.Load += new System.EventHandler(this.AdminForm_Load);
+            this.panel_slide.ResumeLayout(false);
+            this.Panel_subtch.ResumeLayout(false);
+            this.panel_substd.ResumeLayout(false);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel_slide;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button_std;
+        private System.Windows.Forms.Panel panel_substd;
+        private System.Windows.Forms.Button button_stdPrint;
+        private System.Windows.Forms.Button button_manageStd;
+        private System.Windows.Forms.Button button_registrationstd;
+        private System.Windows.Forms.Panel Panel_subtch;
+        private System.Windows.Forms.Button button_printtch;
+        private System.Windows.Forms.Button button_managetch;
+        private System.Windows.Forms.Button button_Registrationtch;
+        private System.Windows.Forms.Button button_tch;
+        private System.Windows.Forms.Label label1;
     }
 }
